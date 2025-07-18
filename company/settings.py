bd0979e49d84a26a80a65186513b0285ac280d64
Sergio Martins <sergiomartins@marista-carcavelos.org>
2025-07-18 09:47:17 +0100

    Versão final do curso

--- company/settings.py
+++ company/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
-]
+]          
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
